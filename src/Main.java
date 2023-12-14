58ed4168a4d079942784b27446f8b5e7461ca116
Gabriela Santos <gbss.santos1707@gmail.com>
2023-12-14 18:25:50 -0300

    iniciando configurações

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Esse é o GabiFlix!");
+        System.out.println("Barbie Moda e Magia");
+
+        int anoDeLancamento = 2010;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+    }
+}
+
